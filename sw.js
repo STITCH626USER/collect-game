80a92b7c234e64f842adf5cc9b56e15be28fae51
STITCH626USER <roux.alexandre13@gmail.com>
2026-08-03 15:24:33 +0200

    v1.72 - Remove inner circle and border around score number in score-badge

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'roi-des-animaux-v1.71';
+const CACHE_NAME = 'roi-des-animaux-v1.72';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css?v=1.71',
-  './game.js?v=1.71',
+  './style.css?v=1.72',
+  './game.js?v=1.72',
   './manifest.json',
   './assets/logo_horizontal.jpg',
   './assets/icon-192.png',
