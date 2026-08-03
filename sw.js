c414116b1af4ec6b8c8657779e2a0618f23cd0c2
STITCH626USER <roux.alexandre13@gmail.com>
2026-08-03 13:33:15 +0200

    v1.59 - Center opponent cards in 1v1 and multiplayer views by default

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'roi-des-animaux-v1.58';
+const CACHE_NAME = 'roi-des-animaux-v1.59';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css?v=1.58',
-  './game.js?v=1.58',
+  './style.css?v=1.59',
+  './game.js?v=1.59',
   './manifest.json',
   './assets/logo_horizontal.jpg',
   './assets/icon-192.png',
