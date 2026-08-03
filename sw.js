ef3806b90832608e038928d4b5584fd46d3ac570
STITCH626USER <roux.alexandre13@gmail.com>
2026-08-03 16:13:13 +0200

    v1.78 - Fix PNG logo orientation to be 100% right-side up and readable

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'roi-des-animaux-v1.77';
+const CACHE_NAME = 'roi-des-animaux-v1.78';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './style.css?v=1.77',
-  './game.js?v=1.77',
+  './style.css?v=1.78',
+  './game.js?v=1.78',
   './manifest.json',
   './assets/logo_horizontal.png',
   './assets/icon-192.png',
